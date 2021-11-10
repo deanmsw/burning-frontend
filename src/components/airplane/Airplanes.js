1d3bed621300b702a5ade0cff4bfa303099e020e
Sarah <sarahjune@gmail.com>
2021-11-10 11:34:20 +1100

    begun airplanes component

--- src/components/airplane/Airplanes.js
+++ src/components/airplane/Airplanes.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react";
+import "./airplanes.css";
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:3000/airplanes.json";
 
 export default class Airplanes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // TODO: replace this via AJAX from the planes server
+      planes: [],
+    };
+    this.savePlane = this.savePlane.bind(this);
+  }
+
+  // react component lifecycle:
+  componentDidMount() {
+    const fetchPlanes = () => {
+      axios(SERVER_URL).then((response) => {
+        this.setState({ planes: response.data });
+        setTimeout(fetchPlanes, 5000);
+      });
+    };
+
+    fetchPlanes();
+  }
+
+  savePlane(content) {
+    axios.post(SERVER_URL, { content: content }).then((response) => {
+      this.setState({ planes: [...this.state.planes, response.data] });
+    });
+  }
+
   render() {
-    return <div>airplane's page</div>;
+    return (
+      <div>
+        <div className="plane-container">
+          <div>Create a new plane</div>
+        </div>
+      </div>
+    );
   }
 }
